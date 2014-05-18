a47e3762a6ef6a793812e59c986af101fee548e4
444Duarte <duartemr.pinto@gmail.com>
2014-05-18 19:50:18 +0100

    CRUD channel terminado/ CRUD program por completar

--- TheBox/Box.cpp
+++ TheBox/Box.cpp
@@ -108,7 +108,7 @@ bool Box::createdChannel(string &n){
 		}
 	}
 	
-	Channel new_channel(n);	// Isto dá erro
+	Channel new_channel(n);	// Isto dá erro    // 17/05/2014- Corrigido o erro. Faltava o construtor na classe
 	channels.push_back(new_channel);
 	saveChannels(channels);
 	return true;
@@ -129,7 +129,144 @@ bool Box::removeChannel(string &n){
 	return false;
 }
 
-//bool updateChannel(){
+bool Box::updateChannel(string &channel_name){
+	system("cls");
+	string new_name;
+	cout << "|||| UPDATE CHANNEL ||||\nChoose the channels new name:\n";
+	getline(cin, new_name);
+	for (int i = 0; i < channels.size(); i++)
+	{
+		if (channels[i].getName() == channel_name)
+		{
+			channels[i].setName(new_name);
+			return true;
+		}
+	}
+	return false;
+}
+
+
+
+// Program CRUD
+
+
+int Box::searchProgram(string &program_name){					// Se encontrar o programa na lista de programas, retorna a posição do programa no vetor, senão retorna -1
+	for (int i = 0; i < recordList.size(); i++)
+	{
+		if (recordList[i].getName() == program_name)
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+
+
+//bool Box::createdProgram(string &channel){
+//	string new_program;
+//	cout << "|||| CREATE PROGRAM ||||";
+//	cout << endl << "Program name:\n";
+//	cin >> new_program;
+//	while (-1!=searchProgram(new_program))
+//	{
+//		system("cls");
+//		cout << "|||| CREATE PROGRAM ||||";
+//		cout << endl << "This name is already in use by another program\n Enter another name:\n";
+//		cin >> new_program;
+//
+//	}
+//
 //	system("cls");
-//	cout << ""
+//	int duration, hour, min;
+//	string type, day;
+//
+//
+//	cout << "Insert the programs:\n";
+//	//INSERIR DURACAO
+//	cout << "Duration(in minutes): ";
+//	cin >> duration;
+//	while (cin.fail() || duration < 1)
+//	{
+//		cin.clear();
+//		cin.ignore(10000, '\n');
+//		cout << "Wrong format. Please insert a number equal or bigger then 1: ";
+//		cin >> duration;
+//	}
+//
+//
+//	//INSERIR HORA INÍCIO
+//
+//	cout << "\nStarting hour: "; cin >> hour;
+//	while (cin.fail() || hour > 23 || hour < 0)
+//	{
+//		cin.clear();
+//		cin.ignore(10000, '\n');
+//		cout << "Wrong format. Please insert a number between 0 and 23: ";
+//		cin >> hour;
+//	}
+//
+//
+//	// INSERIR MINUTOS
+//	cout << "minutes: "; cin >> min;
+//	while (cin.fail() || min > 59 || min < 0)
+//	{
+//		cin.clear();
+//		cin.ignore(10000, '\n');
+//		cout << "Wrong format. Please insert a number between 0 and 59: ";
+//		cin >> min;
+//	}
+//
+//
+//	// INSERIR DIA DA SEMANA
+//	int option;
+//	cout << "Choose a day of the week:\n1.Sunday\n2.Monday\n3.Tuesday\n4.Wednesday\n5.Thursday\n6.Friday\n7.Saturday\n";
+//	cin >> option;
+//	bool repeat;
+//	do
+//	{
+//		repeat = false;
+//		switch (option)
+//		{
+//		case 1:
+//			day = "Sunday";
+//			break;
+//		case 2:
+//			day = "Monday";
+//			break;
+//		case 3:
+//			day = "Tuesday";
+//			break;
+//		case 4:
+//			day = "Wednesday";
+//			break;
+//		case 5:
+//			day = "Thursday";
+//			break;
+//		case 6:
+//			day = "Friday";
+//			break;
+//		case 7:
+//			day = "Saturday";
+//			break;
+//		default:
+//			repeat = true;
+//			cout << "Option mismatch. Please choose a number between 1 and 7: ";
+//			cin >> option;
+//			break;
+//		}
+//	} while (repeat);
+//	
+//	////////////////////////////////////////////
+//	// VERIFICAR A COMPATIBILIDADE DE HORÁRIOS
+//	// por completar
+//	////////////////////////////////////////////
+//
+//
+//	for (int i = 0; i < channels.size(); i++)
+//	{
+//		if (channel==channels[i].getName())
+//		{
+//			channels[i].addProgram();
+//		}
+//	}
 //}
